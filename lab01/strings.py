39bd621eca8a19b8ce3bed7e8c6945d27f914269
Jessica <z5207920@student.unsw.edu.au>
2018-07-27 18:12:31 +1000

    appended all the strings

--- lab01/strings.py
+++ lab01/strings.py
@@ -1,2 +1,12 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
+
+for i in strings:
+	#sentence.append(i)
+	#sentence.append(' ')
+	sentence = sentence + i
+	sentence = sentence + ' '
+print(sentence[:-1])
+
+print(' '.join(strings))
+	
